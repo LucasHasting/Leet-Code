5faf8929658860bcedcb35d96475a2d52e0a2d4e
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2024-01-18 21:43:40 -0600

    Create rotate-list.cpp

---
+++ rotate-list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        //exit if the list is empty
+        if(head == nullptr){
+            return head;
+        }
+
+        //save head and create size variable
+        ListNode* temp = head;
+        int size = 0;
+
+        //get size of list
+        while(head->next != nullptr){
+                head = head->next;
+                ++size;
+        }
+
+        //set head back to original posistion
+        head = temp;
+
+        //rotate the list
+        for(int i = 0; i < k % (size + 1); i++){
+            while(head->next != nullptr){
+                head = head->next;
+                int temp_val = temp->val;
+                temp->val = head->val;
+                head->val = temp_val;
+            }
+            head = temp;
+        }
+
+        //return the head of the list
+        return temp;
+    }
+};
